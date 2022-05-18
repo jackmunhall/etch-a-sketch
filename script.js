0ace0ab606e056221102bbbcf86bb010ac87127e
Jack Munhall <jman777jman@gmail.com>
2022-05-18 01:45:22 -0700

    added slider so user can change grid size b/w 1 and 100

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-let numCells = 16;
+function onSlide() {
+    console.log('entered');
+    const existing = document.querySelectorAll('.cell');
+    for (const elem of existing) {
+        elem.remove();
+    }
 
-for (let i = 0; i < numCells*numCells; i++) {
-    const container = document.querySelector('.container');
-    cell = document.createElement('div');
-    cell.classList.add("cell");
-    container.appendChild(cell);
+    const squares = document.querySelector('.slider');
+    createGrid(squares.value);
+    
+    const label = document.querySelector('label');
+    label.textContent = `${squares.value}`;
+    listen();
 }
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('mouseover', onHover))
+function createGrid(cells) {
+    for (let i = 0; i < cells*cells; i++) {
+        const container = document.querySelector('.container');
+        const cell = document.createElement('div');
+        cell.classList.add("cell");
+        container.appendChild(cell);
+    }
+    const contain = document.querySelector('.container');
+    contain.style.gridTemplateColumns=`repeat(${cells}, 1fr)`;
+    contain.style.gridTemplateRows=`repeat(${cells}, 1fr)`;
+}
 
 function onHover(e) {
-    console.log(e.target);
     e.target.style.backgroundColor = "black";
+}
+
+let numCells = 16;
+createGrid(numCells);
+listen();
+
+function listen() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', onHover))
 }
